5e387df79929a7a3fd0e2ab6f55f51f7a67df3aa
dfsklar <dfsklar@gmail.com>
2014-07-28 21:46:24 -0700

    added access to sugar (sugarjs.com) for easy date manipulation.

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+require("sugar");
 var express = require("express");
 var app = express();
 var http = require("http");
